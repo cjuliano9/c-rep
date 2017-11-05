98ce31d7ed996030bc30122c94ebd4d18bf778c2
Juliano Caroline Charlotte <juliano@IC-CO-IN-SC140.intranet.epfl.ch>
2017-11-05 02:27:18 +0100

    version with doxygen

--- main.cpp
+++ main.cpp
@@ -1,101 +1,16 @@
 #include <iostream>
-#include <fstream>
-#include <random>
-#include <cassert>
-#include "neuron.h"
+#include "experiment.h"
 using namespace std;
 
 
 int main(){
 
-	double J_excitatory(0.1);
-	double J_inhibitory(-0.5);
-
-	int nb_excitatory(10000);
-	int nb_inhibitory(2500);
-	int nb_neurons(nb_excitatory+nb_inhibitory);
-
-	int excitatory_connexion(1000);
-	int inhibitory_connexion(250);
-
-	vector<Neuron> tab_neurons(nb_neurons);
-	//vector<vector<bool> > connexion (nb_neurons*nb_neurons) ;
-
-	for (int i(0);i<nb_neurons; ++i){
-		if (i<nb_excitatory) {
-		tab_neurons[i].setJ(J_excitatory);
-		}
-		else {
-		tab_neurons[i].setJ(J_inhibitory);
-		//excitatory=false;
-		}
-	}
-
-	static random_device device;
-	static mt19937 gen(device());
-	static uniform_int_distribution<> d1(0,9999);
-	static uniform_int_distribution<> d2(10000,12499);
-
-	for (size_t t=0; t<tab_neurons.size();++t){
-
-	for (unsigned int i(0);i<excitatory_connexion;++i){
-		auto target= d1(gen);
-		/*if (target==t){
-			i--;
-		}
-		else{*/
-		tab_neurons[t].setIndex(target);
-		//}
-	}
-
-	for (unsigned int i(0);i<inhibitory_connexion;++i){
-			auto target= d2(gen);
-			/*if (target==t){
-				i--;
-			}
-			else{*/
-			tab_neurons[t].setIndex(target);
-			//}
-		}
-	}
-
-
-	int simtime(0);
-	int spikes;
-
 	const double t_start(0.0);
 	const double t_stop(100.0);
-	ofstream file;
-	bool spike; //=n.update()
-	
-	double time_steps((t_stop-t_start)/0.1);
-	static_cast <int> (time_steps);
-
-	file.open("spikes.txt");
-
-	if(file.fail()){
-		cerr<<"Error when opening the file"<<endl;
-	}	else{
-
-		while (simtime<time_steps){				//changed from time stop to time step
-			for (size_t i(0);i<tab_neurons.size();++i){
-			spike=tab_neurons[i].update(simtime,0.0);			//regarde si spike ou pas (booléen renvoyé par update)
-			
-				if (spike==true){
-					spikes ++;
-				file<</*"Time of spike: "<<*/simtime<<'\t'<</*" Neuron: "<<*/i<<'\n';
-					for (size_t j(0); j<tab_neurons[i].getSize(); ++j){ //getsize renvoie la taille du tableau de target
-					auto t=tab_neurons[i].getIndex(j);
-					tab_neurons[t].receive(simtime,tab_neurons[i].getJ());		//send the signal to the target neuron and register J in buffer
-					}
-				}
-			}
-			simtime++;					//increase global clock
-		}
-	}
-	cerr<<"spike: "<<spikes<<endl;
-
-	file.close();
+	Experiment trial;
+	trial.setTime(t_start, t_stop);
+	trial.simulation();
 
 	return 0;
+
 }
--- neuron.h
+++ neuron.h
@@ -1,5 +1,5 @@
-#ifndef NEURON_HPP
-#define NEURON_HPP
+#ifndef NEURON_H
+#define NEURON_H
 #include <iostream>
 #include <vector>
 #include <cmath>
@@ -13,70 +13,61 @@ class Neuron {
 
 	private:
 
-	double V;		//membrane potential
-	const double V_reset;
-	const double V_thr;
-	const double tau; //ms
-	const double ref_t;
-	const double h;
-	const double R; //resistance R=tau/c
-	const double c1;
-	const	double c2;
-	const double D;
-	double J; //amplitude
-	const double J_ext;
-	double synaptic_current;
+	double V;		//!< membrane potential
+	const double V_reset; //!< reset potential
+	const double V_thres; //!< threshold potential
+	const double tau; //!< in ms
+	const double ref_time; //!< refractory time
+	const double R; //!< resistance
+	const double h; //!< each time step is h
+	const double c1; //!< constant used in potential update
+	const double c2; //!< constant used in potential update
+	const double D; //!< Delay
+	double J; //!< Amplitude of spike
+	const double J_ext; //!< external current from background noise
+	double synaptic_current; //!< background noise + amplitude
 
-	int nb_spikes;
-	vector<double> tab_spikes;
+	int nb_spikes; //!< counter for number of spikes
 
-	int refractory_count;
-	int delay_count;
+	int refractory_count; //!< internal clock to count the refractory period in time steps
+	int delay_count; //!< to count the delay in time steps
 
-	const double nu_ext;
-	const double nu_eta;
+	double nu_eta; //!< ratio of nu_ext/nu_threshold
 
-	vector<double> buffer;
-	int n_clock;
-
-	vector<int> connexion_index;
+	vector<double> buffer; //!< ring buffer used to stock the delays
+	vector<int> connexion_index; //!< tab of the index of neurons targets
 
 	public:
 
-	//bool excitatory;
+	//CONSTRUCTOR/DESTRUCTOR//
 
-	//CONSTRUCTEUR/DESTRUCTEUR//
-
-	Neuron();
-	~Neuron(){
+	Neuron(); //!< constructor
+	~Neuron(){ //!< destructor
 	}
 
-	//GETTER//
+	//GETTERS//
 
-	double getPot() const;
-	int getSpikes() const;
-	//vector<double> getTime() const;
-	double getH() const;
-	double getD() const;
-	int getIndex(int i) const;
-	size_t getSize() const;
-	double getJ() const;
-	//SETTER//
+	double getPot() const; //!< return the potential
+	int getSpikes() const; //!< return number of spikes
+	double getH() const; //!< return the constant h, time step
+	int getD() const; //!< return the delay_count
+	int getIndex(int i) const; //!<return the index of the tab of targets
+	size_t getConnectSize() const; //!< return the size of the tab of targets
+	double getJ() const; //!< return the amplitude J
 
-	void setV(double v);
-	void setJ(double j);
-	void setIndex(int j);
-	//METHODE UPDATE//
+	//SETTERS//
 
-	bool update(int time, double input_current);
+	void setV(double v); //!< set the potential
+	void setJ(double j); //!< set the amplitude
+	void setIndex(int j); //!< set the tab of index
+	void setEta(int eta); //!< set the ratio nu_ext/nu_threshold
+	
+	//OTHER METHODS//
 
+	bool update(int time, double input_current); //!<update the potential of neuron at each time step and return a boolean if spiking
+	void count_spikes(double t);  //!<count the number of spikes and add the time in a tab of spikes
+	void receive(int time, double current);  //!<put the value of current in the buffer of target neuron
 
-	//AUTRES METHODES
-
-	void count_spikes(double t);
-	void receive(int time, double J_);
-
-	//SURCHARGES D'OPERATEURS
 };
 
 #endif
